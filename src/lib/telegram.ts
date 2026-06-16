fdc8075c0ac488cdc3be8cd9a1fc6b2757322cf2
gihasha <dasunfree5@gmail.com>
2026-06-16 17:04:55 +0530

    Add files via upload

---
+++ src/lib/telegram.ts
@@ -0,0 +1,21 @@
+export async function sendTelegram(
+  message:string
+){
+
+  await fetch(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+    {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        chat_id:
+        process.env.TELEGRAM_CHAT_ID,
+
+        text:message
+      })
+    }
+  );
+
+}
