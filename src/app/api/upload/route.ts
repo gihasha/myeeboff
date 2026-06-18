6d531af2d45a13d4dcffc737e4f6fc03d14b9c3a
gihasha <dasunfree5@gmail.com>
2026-06-18 19:32:59 +0530

    Refactor POST handler to accept request parameter

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,9 +1,58 @@
 import { NextResponse } from "next/server";
+import cloudinary from "@/lib/cloudinary";
 
-export async function POST() {
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
 
-  return NextResponse.json({
-    success:true
-  });
+    const file = formData.get("file") as File;
 
-}
+    if (!file) {
+      return NextResponse.json(
+        { error: "No file uploaded" },
+        { status: 400 }
+      );
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const result = await new Promise<any>(
+      (resolve, reject) => {
+
+        cloudinary.uploader
+          .upload_stream(
+            {
+              folder: "dasun-kalhara-users"
+            },
+            (error, result) => {
+
+              if (error) reject(error);
+
+              else resolve(result);
+
+            }
+          )
+          .end(buffer);
+
+      }
+    );
+
+    return NextResponse.json({
+      success: true,
+      imageUrl: result.secure_url
+    });
+
+  } catch (error) {
+
+    return NextResponse.json(
+      {
+        error: "Upload Failed"
+      },
+      {
+        status: 500
+      }
+    );
+
+  }
+              }
