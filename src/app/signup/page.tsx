b3a75774e527b48bd0368c22d691c5aa7ec005b4
gihasha <dasunfree5@gmail.com>
2026-06-20 14:16:53 +0530

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,86 +1,30 @@
-"use client";
+async function signup() {
 
-import { useState } from "react";
+  const res = await fetch(
+    "/api/signup",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      })
+    }
+  );
 
-export default function Signup() {
+  const data = await res.json();
 
-  const [name,setName] =
-  useState("");
+  console.log(data);
 
-  const [email,setEmail] =
-  useState("");
-
-  const [password,setPassword] =
-  useState("");
-
-  async function signup() {
-
-    await fetch(
-      "/api/signup",
-      {
-        method:"POST",
-        headers:{
-          "Content-Type":
-          "application/json"
-        },
-        body:JSON.stringify({
-          name,
-          email,
-          password
-        })
-      }
-    );
-
-    window.location.href =
-    "/login";
+  if (!res.ok) {
+    alert(data.error || "Signup Failed");
+    return;
   }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center">
+  alert("Signup Success");
 
-      <div className="w-[400px] bg-zinc-900 p-8 rounded-xl">
-
-        <h1 className="text-3xl mb-6">
-          Signup
-        </h1>
-
-        <input
-          className="w-full p-3 bg-zinc-800 mb-4"
-          placeholder="Name"
-          onChange={(e)=>
-          setName(
-            e.target.value
-          )}
-        />
-
-        <input
-          className="w-full p-3 bg-zinc-800 mb-4"
-          placeholder="Email"
-          onChange={(e)=>
-          setEmail(
-            e.target.value
-          )}
-        />
-
-        <input
-          type="password"
-          className="w-full p-3 bg-zinc-800 mb-4"
-          placeholder="Password"
-          onChange={(e)=>
-          setPassword(
-            e.target.value
-          )}
-        />
-
-        <button
-          onClick={signup}
-          className="w-full bg-white text-black p-3 rounded-xl"
-        >
-          Create Account
-        </button>
-
-      </div>
-
-    </div>
-  );
-      }
+  window.location.href = "/login";
+}
