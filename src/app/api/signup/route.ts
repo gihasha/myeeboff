8cbcf9b9f024e81041f24e40481117b489a4be09
gihasha <dasunfree5@gmail.com>
2026-06-20 07:35:51 +0530

    Update route.ts

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,40 +2,54 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { hashPassword } from "@/lib/hash";
 
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
+  try {
 
-  const body = await req.json();
+    const body = await req.json();
 
-  const exists =
-    await prisma.user.findUnique({
-      where:{
-        email:body.email
-      }
+    const exists =
+      await prisma.user.findUnique({
+        where: {
+          email: body.email
+        }
+      });
+
+    if (exists) {
+      return NextResponse.json(
+        { error: "Email Exists" },
+        { status: 400 }
+      );
+    }
+
+    const password =
+      await hashPassword(
+        body.password
+      );
+
+    const user =
+      await prisma.user.create({
+        data: {
+          name: body.name,
+          email: body.email,
+          password
+        }
+      });
+
+    return NextResponse.json({
+      success: true,
+      user
     });
 
-  if(exists){
+  } catch (error: any) {
+
     return NextResponse.json(
-      {error:"Email Exists"},
-      {status:400}
+      {
+        error: error.message
+      },
+      {
+        status: 500
+      }
     );
+
   }
-
-  const password =
-    await hashPassword(
-      body.password
-    );
-
-  await prisma.user.create({
-    data:{
-      name:body.name,
-      email:body.email,
-      password
-    }
-  });
-
-  return NextResponse.json({
-    success:true
-  });
-                           }
+          }
